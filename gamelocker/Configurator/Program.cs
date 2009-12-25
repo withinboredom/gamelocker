f5c40c38c1c9db0007fb0ab7d0fdf51ee1516cae
landers.robert <landers.robert@41ceb8d4-f10e-11de-88d6-afc75ad17f39>
2009-12-25 06:10:58 +0000

    Fixed (copyright) yet again ...
    git-svn-id: https://gamelocker.googlecode.com/svn/trunk@7 41ceb8d4-f10e-11de-88d6-afc75ad17f39

--- gamelocker/Configurator/Program.cs
+++ gamelocker/Configurator/Program.cs
@@ -1,4 +1,4 @@
-﻿/*This file is part of GameLocker.
+﻿/* Copyright 2009, 2010 (C) Robert Landers (landers.robert@gmail.com)
 
     GameLocker is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
